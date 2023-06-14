92ed2264304d1eb5238266ff18b7d4e3f525f5e6
Jorge Burgueño <jorge.albert16@hotmail.com>
2023-06-13 20:34:24 -0700

    agregado llenado de pendientes

--- script.js
+++ script.js
@@ -91,23 +91,128 @@ function crearItem(item){
 
 //Forms
 
-const form = document.getElementById('pendientes-form');
-form.addEventListener('submit', callbackFunction);
+class Pendiente {
+    constructor(nombre, fecha, detalles, prioridad) {
+        this.nombre= nombre;
+        this.fecha= fecha;
+        this.detalles= detalles;
+        this.prioridad=prioridad;
+    }
+}
 
-function callbackFunction(event, pendiente) {
-    event.preventDefault();
-    const myFormData = new FormData(event.target);
+class UI {
+    static displayPendiente(){        
+      const pendientes = Store.getPendiente();
 
-    const formDataObj = Object.fromEntries(myFormData.entries());
+      pendientes.forEach((pendiente) => UI.addPendienteALista(pendiente));
+    }
+
+    static addPendienteALista(pendiente){
+       const lista = document.querySelector(".lista-de-pendientes");
+
+       const row = document.createElement("tr");
+       row.innerHTML = `
+       <td>${pendiente.nombre}</td>
+       <td>${pendiente.fecha}</td>
+       <td>${pendiente.detalles}</td>
+       <td>${pendiente.prioridad}</td>
+       <td><button class="btn-delete">X</button></td>
+       `;
+
+       lista.appendChild(row)
+    }
     
-    localStorage.setItem("pendiente",JSON.stringify(pendiente))
-    console.log(formDataObj);
+    static borrarPendiente(el){
+        if(el.classList.contains("btn-delete")){
+           el.parentElement.parentElement.remove() 
+        }
+    }
+    
+
+    static showAlert(mensaje, className){
+        const div = document.createElement("div");
+        div.className = `alert ${className}`;
+        div.appendChild(document.createTextNode(mensaje));
+        const container = document.querySelector(".pendiente-input");
+        const form = document.querySelector("#pendientes-form");
+        container.insertBefore(div, form); 
+        //hacerlo durar solo 2 segundos
+        setTimeout(() => document.querySelector(".alert").remove(), 2000);
+    }
+
+    static clearFields(){
+        document.querySelector("#nombre").value='';
+        document.querySelector("#fecha").value='';
+        document.querySelector("#detalles").value='';
+        document.querySelector("#prioridad").value='';
+    }
 }
 
+//Guardar un pendiente
+class Store {
+    static getPendiente(){
+      let pendientes;
+      if(localStorage.getItem("pendiente") ===null){
+        pendientes = []
+      } else {
+        pendientes = JSON.parse(localStorage.getItem("pendientes"));
+      }
 
-window.onload = function (){
-    display()
+      return pendientes
+    }
+
+    static addPendiente(pendiente){
+      const pendientes = Store.getPendiente();
+      pendientes.push(pendiente);
+      localStorage.setItem("pendientes", JSON.stringify(pendientes));
+    }
+
+    static borraPendiente(nombre){
+      const pendientes = Store.getPendiente();
+
+      pendientes.forEach((pendiente, index) => {
+        if(pendiente.nombre === nombre) {
+            pendientes.splice(index, 1);
+        }
+      });
+      localStorage.setItem("pendientes", JSON.stringify(pendientes));
+    }
 }
+
+//Anadir un pendiente
+document.querySelector("#pendientes-form").addEventListener("submit", (e) =>{
+    
+    e.preventDefault();
+
+    const nombre = document.querySelector("#nombre").value;
+    const fecha = document.querySelector("#fecha").value;
+    const detalles = document.querySelector("#detalles").value;
+    const prioridad = document.querySelector("#prioridad").value;
+
+    if(nombre === "" || fecha === ""  || prioridad === ""){
+      UI.showAlert("Porfavor llena todos los campos")
+    } else {
+        const pendiente = new Pendiente(nombre, fecha, detalles, prioridad);
+    
+        UI.addPendienteALista(pendiente);
+
+        Store.addPendiente(pendiente);
+        
+        UI.clearFields();
+    }    
+
+});
+
+//Borra pendiente
+document.querySelector(".lista-de-pendientes").addEventListener("click", (e) =>{
+    UI.borrarPendiente(e.target);
+    UI.showAlert("Pendiente borrado")
+})
+
+
+
+document.addEventListener("DOMContentLoaded", UI.displayPendiente, display())
+
  
 
 
